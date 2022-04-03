3721ade51738c8d02f643ca15c5ded501183c35d
i-ryzhenkov <i-ryzhenkov@yandex-team.ru>
2022-04-03 13:17:43 +0500

    Исправил ошибки: 1) val теперь выдает по состоянию одно число. 2) max_action вычисляется по актуальной нейронной сети adv, вместо чтения из памяти

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def run_dqn(env, agent, episode_n=150):
     for episode in range(episode_n):
         state = env.reset()
         total_reward = 0
-        for t in range(500):
+        for t in range(env.steps):
             action = agent.get_action(state)
             next_state, reward, done, _ = env.step(action)
             agent.fit(state, action, reward, done, next_state)
@@ -23,16 +23,16 @@ def run_dqn(env, agent, episode_n=150):
 
 
 if __name__ == '__main__':
-    dt = 0.01
+    dt = 0.025
+    episode_n = 500
     simpleEnv = simpleControlProblemDiscrete.SimpleControlProblemDiscrete(dt=dt)
 
     agent = src.DQNAgent.DQNAgent(action_n=simpleEnv.action_n, state_dim=simpleEnv.state_dim, session_duration=simpleEnv.steps)
-    stat_1 = run_dqn(simpleEnv, agent)
+    stat_1 = run_dqn(simpleEnv, agent, episode_n=episode_n)
     print("2 algo")
 
     upd_agent = src.UpdatedDQNAgent.UpdatedDQNAgent(action_n=simpleEnv.action_n, state_dim=simpleEnv.state_dim, session_duration=simpleEnv.steps, dt=dt)
-    stat_2 = run_dqn(simpleEnv, upd_agent)
-
+    stat_2 = run_dqn(simpleEnv, upd_agent, episode_n=episode_n)
 
     plt.plot(stat_1, label="DQN")
     plt.plot(stat_2, label="Modified DQN")
--- src/UpdatedDQNAgent.py
+++ src/UpdatedDQNAgent.py
@@ -22,7 +22,7 @@ class UpdatedDQNAgent(nn.Module):
         self.batch_size = 64
         self.learinig_rate = 1e-2
 
-        self.val = network.Network(self.state_dim, self.action_n)
+        self.val = network.Network(self.state_dim, 1)
         self.adv = network.Network(self.state_dim, self.action_n)
         self.val_optimizer = torch.optim.Adam(self.val.parameters(), lr=self.learinig_rate)
         self.adv_optimizer = torch.optim.Adam(self.adv.parameters(), lr=self.learinig_rate)
@@ -39,8 +39,7 @@ class UpdatedDQNAgent(nn.Module):
         return action
 
     def fit(self, state, action, reward, done, next_state):
-        max_action = self.calculate_max_action(state)
-        self.memory.append([state, action, max_action, reward, done, next_state])
+        self.memory.append([state, action, reward, done, next_state])
         if len(self.memory) > self.memory_size:
             self.memory.pop(0)
 
@@ -53,28 +52,23 @@ class UpdatedDQNAgent(nn.Module):
             """
             batch = random.sample(self.memory, self.batch_size)
 
-            states, actions, max_actions, rewards, dones, next_states = list(zip(*batch))
+            states, actions, rewards, dones, next_states = list(zip(*batch))
             states = torch.FloatTensor(states)
             actions = torch.LongTensor(actions).unsqueeze(1)
             next_states = torch.FloatTensor(next_states)
-            max_actions = torch.LongTensor(max_actions).unsqueeze(1)
             rewards = torch.FloatTensor(rewards).unsqueeze(1)
 
-            v_data = self.val(states)
-            next_v_data = self.calculate_next_v(dones, next_states)
+            v = self.val(states)
+            next_v = self.calculate_next_v(dones, next_states)
             pre_advs_data = self.adv(states)
-            pre_max_advs_data = self.adv(states)
             pre_advs = torch.FloatTensor(self.batch_size)
             pre_max_advs = torch.FloatTensor(self.batch_size)
-            v = torch.FloatTensor(self.batch_size)
-            next_v = torch.FloatTensor(self.batch_size)
             for i in range(self.batch_size):
+                max_action = torch.argmax(pre_advs_data[i])
                 pre_advs[i] = pre_advs_data[i][actions[i]]
-                pre_max_advs[i] = pre_max_advs_data[i][max_actions[i]]
-                v[i] = v_data[i][actions[i]]
-                next_v[i] = next_v_data[i][actions[i]]
+                pre_max_advs[i] = pre_advs_data[i][max_action]
             adv = pre_advs - pre_max_advs
-
+            adv = adv.reshape(self.batch_size, 1)
             q_values = v + self.dt * adv
             expected_q_values = (rewards * self.dt + self.gamma ** self.dt * next_v).detach()
             critic_value = (q_values - expected_q_values) ** 2
